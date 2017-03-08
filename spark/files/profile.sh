9db98a59e44dc350f3c1b59cc671aa1ff6f1324b
Jack Scott <jack@nine78.com>
2017-03-08 00:44:56 -0500

    got master service working, added worker service

--- spark/files/profile.sh
+++ spark/files/profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 {% from "spark/map.jinja" import spark with context %}
-[[ ":$PATH:" == *":{{ spark.bin_dir }}:"* ]] || export PATH=$PATH:{{ spark.bin_dir }}
+[[ ":$PATH:" == *":{{ spark.real_root }}:"* ]] || export PATH=$PATH:{{ '%s/bin:%s/sbin'|format(spark.real_root, spark.real_root) }}
 export SPARK_HOME={{ spark.real_root }}
 export SPARK_CONF_DIR={{ spark.config_dir }}
 
