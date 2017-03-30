6d181da5e8d72880e55b0e617e89016e938e65af
Jack Scott <jack@nine78.com>
2017-03-29 20:36:43 -0400

    fix master uri

--- spark/files/profile.sh
+++ spark/files/profile.sh
@@ -5,4 +5,4 @@ export SPARK_HOME={{ spark.alt_root }}
 export SPARK_CONF_DIR={{ spark.config_dir }}
 export SPARK_MASTER_HOST={{ spark.master_host }}
 export SPARK_MASTER_PORT={{ spark.master_port }}
-export SPARK_MASTER_URI=salt://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT
+export SPARK_MASTER_URI=spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT
