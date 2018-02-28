763d02a022fa4455405accd817199babefd5b8c7
Jack Scott <jack@nine78.com>
2018-02-28 11:14:41 -0500

    allow grains to override the pillar data for master-{host,port}, config_dir etc

--- test/integration/default/controls/master.rb
+++ test/integration/default/controls/master.rb
@@ -13,11 +13,17 @@ control 'spark-master-ports' do
   impact 1.0
   desc  'Server: Checks for UI and Admin ports to be open.'
   
-  portlist = [8080,7077]
+  portlist = [8080,12345]
   for p in portlist do
     describe port(p) do
       it { should be_listening }
     end
   end
+
+  # we override spark:lookup:master_port in a grain, assert the default port is not being listened to
+  describe port(7707) do
+    it { should_not be_listening }
+  end
+  
 end
 
